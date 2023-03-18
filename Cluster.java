ebcef99109b888bc59c3b800d396631587488fde
DakshG <dakshgopalani@gmail.com>
2023-03-17 19:21:35 -0700

    added k means function (its an outline)

--- Cluster.java
+++ Cluster.java
@@ -26,4 +26,47 @@ public class Cluster {
         }
         return Math.sqrt(squareSum) / clusterMushrooms.size();
     }
+
+    public static ArrayList<Cluster> kMeans(ArrayList<Mushroom> mushrooms, int k, int maxIterations) {
+        // Initialize k clusters with random centroids
+        ArrayList<Cluster> clusters = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            int randomIndex = (int) (Math.random() * mushrooms.size());
+            Mushroom randomMushroom = mushrooms.get(randomIndex);
+            Cluster cluster = new Cluster(randomMushroom);
+            clusters.add(cluster);
+        }
+    
+        // Perform k-means iterations
+        for (int iteration = 0; iteration < maxIterations; iteration++) {
+            // Assign each mushroom to the nearest cluster
+            for (Mushroom mushroom : mushrooms) {
+                double minDistance = Double.MAX_VALUE;
+                Cluster nearestCluster = null;
+                for (Cluster cluster : clusters) {
+                    double distance = mushroom.findCombinedDistance(cluster.centroid);
+                    if (distance < minDistance) {
+                        minDistance = distance;
+                        nearestCluster = cluster;
+                    }
+                }
+                nearestCluster.clusterMushrooms.add(mushroom);
+            }
+    
+            // Update centroids of each cluster
+            for (Cluster cluster : clusters) {
+                if (!cluster.clusterMushrooms.isEmpty()) {
+                    Mushroom newCentroid = calculateCentroid(cluster.clusterMushrooms);
+                    cluster.centroid = newCentroid;
+                    cluster.clusterMushrooms.clear();
+                }
+            }
+        }
+    
+        return clusters;
+    }
+    
+    private static Mushroom calculateCentroid(ArrayList<Mushroom> mushrooms) {
+        //get mushroom in center from list of mushrooms
+    }
 }
